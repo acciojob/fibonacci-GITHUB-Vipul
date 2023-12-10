8d563d8c1d273e1b1ba95013633cab1f2d311ef1
GITHUB-Vipul <126069867+GITHUB-Vipul@users.noreply.github.com>
2023-12-10 21:26:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
+
+function fibonacciHelper(num,arr)
+	{
+		if(num==1) return arr[0];
+		if(num==2||num==3) return arr[1];
+		if(arr[num]!=undefined) return arr[num];
+		return arr[num]=fibonacciHelper(num-1,arr)+fibonacciHelper(num-2,arr);
+	}
 function fibonacci(num) {
 // your code here
-	if(num==1) return 0;
-	if(num==2||num==3) return 1;
-	return fibonacci(num-1)+fibonacci(num-2);
+	let arr=new Array();
+	arr.push(0);
+	arr.push(1);
+	arr.push(1);
+	return fibonacciHelper(num,arr);
 }
 
 module.exports = fibonacci;
